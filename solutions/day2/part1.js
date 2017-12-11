d659e01ee585d2db113521c8bb4244242adaba53
Qwerty-Space <oliver_duce@live.co.uk>
2017-12-11 18:34:10 +0000

    refactored day2 part1

--- solutions/day2/part1.js
+++ solutions/day2/part1.js
@@ -1,15 +1,7 @@
 require("fs").readFile(require("path").resolve(__dirname, "input"), "utf8", (err, str) => {    
-    let rows = str.split(/\r?\n/);
-    let checksum = 0;
-    console.log(rows);
-    for(let row of rows) {
-        console.log(row);
+    let result = str.split(/\r?\n/).reduce((sum, row) => {
         let numbers = row.split("\t").map(Number);
-        console.log(numbers);
-        let max = Math.max.apply(null, numbers);
-        let min = Math.min.apply(null, numbers);
-        let dif = max - min;
-        checksum += dif;
-    }
-    console.log(checksum);
+        return sum + Math.max.apply(null, numbers) - Math.min.apply(null, numbers);        
+    },0)
+    console.log(result);
 });
